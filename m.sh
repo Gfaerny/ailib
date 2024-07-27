51683cb607054f18a42783d90c79254cf78c8a1c
gfaerny <120312426+Gfaerny@users.noreply.github.com>
2024-07-27 06:53:16 -0700

    Update m.sh

--- m.sh
+++ m.sh
@@ -19,10 +19,37 @@
         sed -i '1i #include <iostream>' "$file"
         break
     fi
-
+    if grep -q "std::clog" "$file"; then
+        sed -i '1i #include <iostream>' "$file"
+        break
+    fi
+    if grep -q "std::cerr" "$file"; then
+        sed -i '1i #include <iostream>' "$file"
+        break
+    fi
+    if grep -q "std::wcin" "$file"; then
+        sed -i '1i #include <iostream>' "$file"
+        break
+    fi
+    if grep -q "std::wcout" "$file"; then
+        sed -i '1i #include <iostream>' "$file"
+        break
+    fi
+    
     done
 
+# # End of iostream stm
 
+# # statement that depend sstream lib
 
-
+for  i in 1;
+    do
+   
+    if grep -q "std::stringbuf" "$file"; then
+        sed -i '1i #include <sstream>' "$file"
+        break
+    fi
+   
+    
+    done
 # # IN PROCESS
