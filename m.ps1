ce480836db63c03913c6a043a7e3e028776e9548
gfaerny <120312426+Gfaerny@users.noreply.github.com>
2024-07-27 06:01:08 -0700

    Update m.ps1

--- m.ps1
+++ m.ps1
@@ -1,1 +1,13 @@
+#  Gfaerny
+# # AILIB shell script App that provide standard library
+# # No license
+# #for cpp
 
+param ($file)
+
+# For iostream lib
+if (Get-Content $file | Select-String -Quiet "std::cout") {
+    Add-Content $file -Value "#include <iostream>" -Force
+} elseif (Get-Content $file | Select-String -Quiet "std::cin") {
+    Add-Content $file -Value "#include <iostream>" -Force
+}
