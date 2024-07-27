1c2f6ead1912e3b919d0fd54416c86110b6c16e9
gfaerny <120312426+Gfaerny@users.noreply.github.com>
2024-07-27 05:59:35 -0700

    Update m.sh

--- m.sh
+++ m.sh
@@ -1,1 +1,28 @@
+#!/bin/bash
+#   Gfaerny
+# # AILIB shell script App that provide  standard library
+# # No lisence 
+# # for cpp
 
+    file="$1"
+   
+   
+# # For iostream lib
+    for  i in 1;
+    do
+   
+    if grep -q "std::cout" "$file"; then
+        sed -i '1i #include <iostream>' "$file"
+        break
+    fi
+    if grep -q "std::cin" "$file"; then
+        sed -i '1i #include <iostream>' "$file"
+        break
+    fi
+
+    done
+
+
+
+
+# # IN PROCESS
