199c175f78e65759893c9181e61a1e285f18886e
gfaerny <120312426+Gfaerny@users.noreply.github.com>
2024-07-28 13:52:32 -0700

    Update m.ps1

--- m.ps1
+++ m.ps1
@@ -2,12 +2,89 @@
 # # AILIB shell script App that provide standard library
 # # No license
 # # for cpp
-
-param ($file)
+$file = $args[0]
 
 # For iostream lib
-if (Get-Content $file | Select-String -Quiet "std::cout") {
-    Add-Content $file -Value "#include <iostream>" -Force
-} elseif (Get-Content $file | Select-String -Quiet "std::cin") {
-    Add-Content $file -Value "#include <iostream>" -Force
+for ($i = 1; $i -le 1; $i++) {
+    if (Get-Content -Path $file -ErrorAction SilentlyContinue | Select-String -Pattern "std::cout") {
+        Add-Content -Path $file -Value "#include <iostream>"
+        break
+    }
+    if (Get-Content -Path $file -ErrorAction SilentlyContinue | Select-String -Pattern "std::cin") {
+        Add-Content -Path $file -Value "#include <iostream>"
+        break
+    }
+    if (Get-Content -Path $file -ErrorAction SilentlyContinue | Select-String -Pattern "std::clog") {
+        Add-Content -Path $file -Value "#include <iostream>"
+        break
+    }
+    if (Get-Content -Path $file -ErrorAction SilentlyContinue | Select-String -Pattern "std::cerr") {
+        Add-Content -Path $file -Value "#include <iostream>"
+        break
+    }
+    if (Get-Content -Path $file -ErrorAction SilentlyContinue | Select-String -Pattern "std::wcin") {
+        Add-Content -Path $file -Value "#include <iostream>"
+        break
+    }
+    if (Get-Content -Path $file -ErrorAction SilentlyContinue | Select-String -Pattern "std::wcout") {
+        Add-Content -Path $file -Value "#include <iostream>"
+        break
+    }
 }
+
+# End of iostream stm
+
+# statement that depend sstream lib
+for ($i = 1; $i -le 1; $i++) {
+    if (Get-Content -Path $file -ErrorAction SilentlyContinue | Select-String -Pattern "std::stringbuf") {
+        Add-Content -Path $file -Value "#include <sstream>"
+        break
+    }
+}
+
+# For vector '
+for ($i = 1; $i -le 1; $i++) {
+    if (Get-Content -Path $file -ErrorAction SilentlyContinue | Select-String -Pattern "std::vector") {
+        Add-Content -Path $file -Value "#include <vector>"
+        break
+    }
+    if (Get-Content -Path $file -ErrorAction SilentlyContinue | Select-String -Pattern "std::hash") {
+        Add-Content -Path $file -Value "#include <vector>"
+        break
+    }
+    if (Get-Content -Path $file -ErrorAction SilentlyContinue | Select-String -Pattern "std::begin") {
+        Add-Content -Path $file -Value "#include <vector>"
+        break
+    }
+    if (Get-Content -Path $file -ErrorAction SilentlyContinue | Select-String -Pattern "std::cbegin") {
+        Add-Content -Path $file -Value "#include <vector>"
+        break
+    }
+    if (Get-Content -Path $file -ErrorAction SilentlyContinue | Select-String -Pattern "std::end") {
+        Add-Content -Path $file -Value "#include <vector>"
+        break
+    }
+    if (Get-Content -Path $file -ErrorAction SilentlyContinue | Select-String -Pattern "std::cend") {
+        Add-Content -Path $file -Value "#include <vector>"
+        break
+    }
+}
+
+# for algorithm 
+for ($i = 1; $i -le 1; $i++) {
+    if (Get-Content -Path $file -ErrorAction SilentlyContinue | Select-String -Pattern "std::min") {
+        Add-Content -Path $file -Value "#include <algorithm>"
+        break
+    }
+    if (Get-Content -Path $file -ErrorAction SilentlyContinue | Select-String -Pattern "std::max") {
+        Add-Content -Path $file -Value "#include <algorithm>"
+        break
+    }
+    if (Get-Content -Path $file -ErrorAction SilentlyContinue | Select-String -Pattern "std::sort") {
+        Add-Content -Path $file -Value "#include <algorithm>"
+        break
+    }
+}
+
+echo ""
+# IN PROCESS
